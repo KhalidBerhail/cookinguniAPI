4aab22004e13eeeb0bc01d16bd97770e1a37400e
Kurolo <khalidbe3363@gmail.com>
2021-07-16 14:32:39 +0200

    added some middlewares for error handling.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-const productRoutes = require('./api/routes/products');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
-const orderRoutes = require('./api/routes/orders');
+app.use(morgan("dev"));
 
-//midle-wear for http requests starting  with /products
-app.use('/products', productRoutes);
+//middleware for http requests starting  with /products
+app.use("/products", productRoutes);
 
-//midle-wear for http requests starting with /orders
-app.use('/orders', orderRoutes);
+//middleware for http requests starting with /orders
+app.use("/orders", orderRoutes);
+
+//middleware to handle errors (incase none of the other midle-wears were reached)
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+//middleware to handle all errors thrown by the app
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error:{
+        message: error.message
+    }
+  });
+});
 
 module.exports = app;
